dc7293ed310814fa59a832321fdeabab30d6b4d7
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-10-04 17:57:48 +0200

    Fix prepend list

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -6,5 +6,5 @@ defmodule ListLength do
   def call(list), do: get_length(list, 0)
 
   defp get_length([], acc), do: acc
-  defp get_length([_head, tail], acc), do: get_length(tail, 1 + acc)
+  defp get_length([_head | tail], acc), do: get_length(tail, 1 + acc)
 end
