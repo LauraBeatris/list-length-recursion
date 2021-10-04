75262e21336c4ef1294aa2575c1241385ee04d44
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-10-04 16:10:14 +0200

    Install credo for listing

--- mix.exs
+++ mix.exs
@@ -21,6 +21,7 @@ defmodule ListLength.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:json_response, git: "https://github.com/joaopealves/json_response"}
     ]
   end
